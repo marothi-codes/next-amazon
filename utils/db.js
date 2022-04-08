b68955810c61aa049a2069f1dc6ab09c336171e0
Marothi Mahlake <code_artist@outlook.com>
2022-04-08 11:14:26 +0200

    JsDoc comments added to db.js

--- utils/db.js
+++ utils/db.js
@@ -1,12 +1,17 @@
 import mongoose from 'mongoose';
 
-const connection = {};
+const connection = {}; // Database connection properties are stored in this object.
 
+/**
+ * Connects to the database if no exsisting connections are detected.
+ * @returns void
+ */
 async function connect() {
   if (connection.isConnected) {
     console.log('already connected');
-    return;
+    return; // Exits the function.
   }
+  // Check for existing connections, don't connect if already connected.
   if (mongoose.connections.length > 0) {
     connection.isConnected = mongoose.connections[0].readyState;
     if (connection.isConnected === 1) {
@@ -15,6 +20,7 @@ async function connect() {
     }
     await mongoose.disconnect();
   }
+  // Establish a connection to the database.
   const db = await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -24,7 +30,11 @@ async function connect() {
   connection.isConnected = db.connections[0].readyState;
 }
 
+/**
+ * Terminates the database connection in a production environment.
+ */
 async function disconnect() {
+  // Check for connections and disconnect if running in production.
   if (connection.isConnected) {
     if (process.env.NODE_ENV === 'production') {
       await mongoose.disconnect();
@@ -34,7 +44,11 @@ async function disconnect() {
     }
   }
 }
-
+/**
+ * Converts a database document into a JSON object.
+ * @param {object} doc The database object.
+ * @returns {object} JSON representation of the document.
+ */
 function convertDocToObj(doc) {
   doc._id = doc._id.toString();
   doc.createdAt = doc.createdAt.toString();
